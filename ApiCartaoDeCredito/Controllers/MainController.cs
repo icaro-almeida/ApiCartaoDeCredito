7a92e89dcd2def24bf2aa0707ca61cd322e7a14b
Icaro <icaro.almeida@engenharia.ufjf.br>
2021-05-28 22:39:36 -0300

    Criação do método POST na MainController

--- ApiCartaoDeCredito/Controllers/MainController.cs
+++ ApiCartaoDeCredito/Controllers/MainController.cs
@@ -34,6 +34,36 @@ namespace ApiCartaoDeCredito.Controllers
             return ccList;
         }
 
+        // POST: api/Main
+        [HttpPost("{email}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<CreditCard>> PostCreditCard(string email)
+        {
+            if (!DataHelper.IsValidEmail(email))
+                return BadRequest();
+
+            Person person = await _context.People.FirstOrDefaultAsync(i => i.Email == email);
+
+            if (person == null)
+            {
+                person = new Person
+                {
+                    Email = email
+                };
+                _context.People.Add(person);
+                await _context.SaveChangesAsync(); //Adds person to db and gets auto incremented PersonId                
+            }
+
+            var newCC = DataHelper.GenerateCreditCard(person.PersonId, expYears: 10);
+
+            _context.CreditCards.Add(newCC);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCreditCards), new { email = person.Email }, newCC);
+        }
+
+
 
     }
 }
